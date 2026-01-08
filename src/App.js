78d7277e26efb6f5968f53888dfb11c92fbd4efa
Sirapat903 <s6703052411368@email.kmutnb.ac.th>
2026-01-08 11:43:48 +0700

    EventData2

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import { Calculator2 } from './calculator';
 import  Calendar from './class-components';
 
 import React from 'react'
+import { EventData2 } from './event-data';
 
 function App() {
-return <Calculator2/>
+return <EventData2/>
   }
 
 
